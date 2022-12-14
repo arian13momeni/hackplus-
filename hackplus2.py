413d34d000114fa36155b6358735fc814277d89b
ariAN <114738229+arian13momeni@users.noreply.github.com>
2022-12-14 14:45:50 +0330

    Add files via upload

---
+++ hackplus2.py
@@ -0,0 +1,19 @@
+from pyfiglet import*
+print(figlet_format('hack plus +'))
+
+print('_____________________________________')
+
+print('welcom to the hack plus')
+print('_____________________________________')
+print('1.hack massnger')
+print('_____________________________________')
+print('2.hack websit')
+choose = input("Choose an option:")
+if (str(choose) == "1"):
+    choose = input('phon number:')
+    print(figlet_format('hack shod'))
+    print('ramz be email shoma ersal shod')
+elif (str(choose) == "2"):
+    choose = input('url:')
+    print(figlet_format('hack shod'))
+    print('ramz be email shoma ersal shod')
